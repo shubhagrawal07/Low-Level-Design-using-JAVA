49a3368252ecaf00faeec1e5a9e6bceae27d979a
Shubh Agrawal <shubhagrawal0710@gmail.com>
2026-02-17 03:03:19 +0530

    hello world and variables in java

---
+++ Java Basics/CoreSyntax/variables/variable.java
@@ -0,0 +1,24 @@
+public class variable {
+
+    public static void main(String[] arg){
+        int a = 1;
+        float b = 2.33f;
+        double c = 3.14;
+        boolean d = false;
+        char e = 'K';
+        long f = 4455l;
+
+        int binary = 0b1010;
+        int hexa = 0x1C9;
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+
+        System.out.println(binary);
+        System.out.println(hexa);
+    }  
+}
